98ad37db5155ca891be171fd461d8b88d3187b70
Bhavesh Agrawal <bhavesh.agrawal242@gmail.com>
2021-10-30 20:53:19 -0700

    Added functionality to move draggable items accross columns

--- src/index.js
+++ src/index.js
@@ -4,58 +4,99 @@ import initialData from './initial-data';
 import Column from './column';
 import '@atlaskit/css-reset';
 import { DragDropContext } from 'react-beautiful-dnd';
+import styled from 'styled-components';
 
+const Container = styled.div`
+    display: flex;
+
+`;
 class App extends React.Component {
     state = initialData
 
     onDragEnd = result => {
         const { destination, source, draggableId } = result;
-    
+
         if (!destination) {
-          return;
+            return;
         }
-    
+
+        //If Location of the Draggable item is unchanged
         if (
-          destination.droppableId === source.droppableId &&
-          destination.index === source.index
+            destination.droppableId === source.droppableId &&
+            destination.index === source.index
         ) {
-          return;
+            return;
         }
-    
-        const column = this.state.columns[source.droppableId];
-        const newTaskIds = Array.from(column.taskIds);
-        newTaskIds.splice(source.index, 1);
-        newTaskIds.splice(destination.index, 0, draggableId);
-    
-        const newColumn = {
-          ...column,
-          taskIds: newTaskIds,
+
+        
+        const start = this.state.columns[source.droppableId];
+        const finish = this.state.columns[destination.droppableId];
+        //If draggable item shifts location within the same column
+        if (start === finish) {
+            const newTaskIds = Array.from(start.taskIds);
+            newTaskIds.splice(source.index, 1);
+            newTaskIds.splice(destination.index, 0, draggableId);
+
+            const newColumn = {
+                ...start,
+                taskIds: newTaskIds,
+            };
+
+            const newState = {
+                ...this.state,
+                columns: {
+                    ...this.state.columns,
+                    [newColumn.id]: newColumn,
+                },
+            };
+
+            this.setState(newState);
+            return;
+        }
+
+        //Moving to a different column
+        const startTaskIds = Array.from(start.taskIds)
+        startTaskIds.splice(source.index,1);
+        const newStart = {
+            ...start,
+            taskIds: startTaskIds,
         };
-    
+        const finishTaskIds = Array.from(finish.taskIds)
+        finishTaskIds.splice(source.index,0,draggableId);
+        const newFinish = {
+            ...finish,
+            taskIds: finishTaskIds,
+        };
+
         const newState = {
-          ...this.state,
-          columns: {
-            ...this.state.columns,
-            [newColumn.id]: newColumn,
-          },
-        };
-    
-        this.setState(newState); 
-      };
-    
+            ...this.state,
+            columns: {
+                ...this.state.columns,
+                [newStart.id]: newStart,
+                [newFinish.id]: newFinish
+            }
+        }
+
+        this.setState(newState)
+    };
+
 
     render() {
         return (
-          <DragDropContext onDragEnd={this.onDragEnd}>
-            {this.state.columnOrder.map(columnId => {
-              const column = this.state.columns[columnId];
-              const tasks = column.taskIds.map(taskId => this.state.tasks[taskId]);
-    
-              return <Column key={column.id} column={column} tasks={tasks} />;
-            })}
-          </DragDropContext>
+
+            <DragDropContext onDragEnd={this.onDragEnd}>
+                <Container>
+                    {this.state.columnOrder.map(columnId => {
+                        const column = this.state.columns[columnId];
+                        const tasks = column.taskIds.map(taskId => this.state.tasks[taskId]);
+
+                        return <Column key={column.id} column={column} tasks={tasks} />;
+                    })}
+                </Container>
+            </DragDropContext>
+
         );
-      }
     }
+}
 
 ReactDOM.render(<App />, document.getElementById('root'));
