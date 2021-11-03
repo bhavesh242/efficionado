90506024dd08ceb6f1d154225f50883a401319cf
Bhavesh Agrawal <bhavesh.agrawal242@gmail.com>
2021-11-02 22:11:41 -0700

    Added Dependencies and Config to access MongoDB ATLAS

--- backend/server.js
+++ backend/server.js
@@ -1,20 +1,13 @@
-import connectDB from './config/db.js'
-import userRoutes from './routes/userRoute.js'
 import express from 'express'
-import dotenv  from 'dotenv'
+import mongoose from 'mongoose'
+import config from 'config';
 
-//connect database
-connectDB()
+const app = express();
+const dbConfig = config.get('Efficionado.dbConfig.dbName')
 
-//dotenv config
-dotenv.config()
-
-const app = express()
-
-//Creating API for user
-app.use('/api/users', userRoutes)
-
-const PORT = process.env.PORT || 5000
-
-//Express js listen method to run project on http://localhost:5000
-app.listen(PORT, console.log(`App is running in ${process.env.NODE_ENV} mode on port ${PORT}`))
+mongoose.connect(dbConfig, {
+}).then(()=>{
+    console.log('Database Connected');
+}).catch(err=>{
+    console.log(err)
+});
