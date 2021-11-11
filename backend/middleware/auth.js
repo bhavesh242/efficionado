2157a1979792c3c06a58824257228a37510c2ca6
Bhavesh Agrawal <bhavesh.agrawal242@gmail.com>
2021-11-10 22:14:21 -0800

    Fixed most of the linting errors from the backend code

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -1,23 +1,20 @@
 const jwt = require('jsonwebtoken');
-const responsBuilder = require('../utils/responseBuilder')
 const config = require('config');
+const responsBuilder = require('../utils/responseBuilder');
 const responseBuilder = require('../utils/responseBuilder');
 
 const auth = (req, res, next) => {
-    try {
-        const token = req.header("x-auth-token");
-        if (!token)
-            return responsBuilder(res, 401, { msg: "No authentication token, access denied" });
-        const verified = jwt.verify(token, config.get('Efficionado.jwtSecret'));
-        if (!verified)
-            return responseBuilder(res, 401, { msg: "Token verification failed, authorization denied" });
+  try {
+    const token = req.header('x-auth-token');
+    if (!token) return responsBuilder(res, 401, { msg: 'No authentication token, access denied' });
+    const verified = jwt.verify(token, config.get('Efficionado.jwtSecret'));
+    if (!verified) return responseBuilder(res, 401, { msg: 'Token verification failed, authorization denied' });
 
-        req.user = verified.id;
-        next();
-    } catch (err) {
-        return responseBuilder(res, 500, { error: err.message });
-    }
-
-}
+    req.user = verified.id;
+    return next();
+  } catch (err) {
+    return responseBuilder(res, 500, { error: err.message });
+  }
+};
 
 module.exports = auth;
